63329c35c21407c41866ace1102c951d301d2b99
UgisOzolins <ugis.ozolins01@gmail.com>
2025-01-07 21:21:51 +0200

    Ievada tapesu garumus

--- Saraksts.py
+++
@@ -1,6 +0,0 @@
-# Saraksts ar tapesu datiem
- 
-tapesu_dati = [
-    [float(input("Ievadi pirma tapesu rulla garumu (metri): ")), float(input("Ievadi  pirma tapesu rulla platumu (metri): "))],    # pieprasa ievadit mainigajiem skaitu 
-    [float(input("Ievadi otra tapesu rulla garumu (metri): ")), float(input("Ievadi otra tapesu rulla platumu (metri): "))]
-]
