ec4efed0b2ec74959e92509acd7cf732c4c971ac
UgisOzolins <ugis.ozolins01@gmail.com>
2025-01-07 21:12:55 +0200

    Programma aprekina cenu pec visiem ievaditajiem datiem

---
+++ Cena aprekins.py
@@ -0,0 +1,4 @@
+cena = float(input("Ievadi tapesu rulla cenu: "))
+
+print("Kopejas tapesu izmaksas:")
+print(f"{tapesu_izmaksa(cena, tapesu_dati, telpas_dati):.2f} EUR")              # izvada izmaksu pec ievaditajiem datiem
